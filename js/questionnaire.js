11f756b5b3e33cb59202b4d1e75035ba84efe4cb
Christoffer Bjorkskog <christoffer.bjorkskog@genero.fi>
2015-04-10 11:58:56 +0200

    Enabled you to have a 'pre-view' that you show before you start the quiz. Also enabled you to register callbacks you want to run when you press the start button and when you have finished... if you are themeing something special. such as making the background blurred when you start etc...

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -3,13 +3,44 @@
     attach: function (context, settings) {
 
       $(document).ready(function() {
+        $("body").once(function() {
+
 
         var $questionsForm = $('#questionnaire-question-form')
           , $questions     = $questionsForm.find('.form-type-radios')
           , $submit        = $questionsForm.find('.form-submit')
           , $result        = $questionsForm.find('.result')
           , $progress      = $questionsForm.find('.progress')
-          , count          = $questions.length;
+          , count          = $questions.length
+          , $preView       = $('#questionnaire-pre-view')
+          , previewEnabled = $preView.length > 0
+          , i
+          , len;
+
+        // if we haven't submitted yet and we have a "pre-view" to show before the quiz starts...
+        if($questionsForm.find('.result-text').length == 0 && previewEnabled &&  $('#questionnaire-pre-view .start-button').length > 0) {
+          $questionsForm.hide();
+
+          $('#questionnaire-pre-view .start-button').click(function() {
+            $preView.hide();
+            $questionsForm.show();
+
+            if (afterStartFunctions && afterStartFunctions.length > 0) {
+              for (i = 0, len = afterStartFunctions.length; i < len; i++) {
+                afterStartFunctions[i]();
+              };
+            }
+          });
+
+        // if we have submitted and then want to run the afterEndFunctions we make available...
+        } else {
+          $preView.hide();
+          if (afterEndFunctions && afterEndFunctions.length > 0) {
+            for (i = 0, len = afterEndFunctions.length; i < len; i++) {
+              afterEndFunctions[i]();
+            };
+          }
+        }
 
         $submit.hide();
         $result.fadeIn('100');
@@ -36,7 +67,6 @@
           else {
             $questionsForm.submit();
           }
-
         });
 
         // Count character length in textarea and limit to max length.
@@ -53,7 +83,6 @@
           if (maxLength <= currLength) {
             event.preventDefault();
           }
-
         });
 
         $('.share-btn').click(function(event) {
@@ -68,9 +97,25 @@
             href: url,
           }, function(response) {});
         }
-
       });
+     });
 
     }
   };
+
+  Drupal.questionnaire = Drupal.questionnaire || {};
+
+  var afterStartFunctions = [];
+  var afterEndFunctions = [];
+
+  Drupal.questionnaire.afterStart = function(fn) {
+    afterStartFunctions.push(fn);
+  };
+
+  Drupal.questionnaire.afterEnd = function(fn) {
+    afterEndFunctions.push(fn);
+  };
+
+
+
 })(jQuery);
